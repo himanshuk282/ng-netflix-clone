96ba6eb4e079bc8fe3e7a7b2d9856466b4f4b43d
Himanshu Singh <himanshuk282@gmail.com>
2024-04-05 22:07:30 +0530

    feature: banner component changes

--- src/app/pages/browse/browse.component.ts
+++ src/app/pages/browse/browse.component.ts
@@ -1,11 +1,12 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { AuthService } from '../../shared/services/auth.service';
 import { HeaderComponent } from '../../core/components/header/header.component';
+import { BannerComponent } from '../../core/components/banner/banner.component';
 
 @Component({
   selector: 'app-browse',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,BannerComponent],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.css'
 })
